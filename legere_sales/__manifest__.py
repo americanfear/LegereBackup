88a477a81b529550012c3b1e61c9fd136a7f932c
Andrew Hastings <andrew@dreammtn.services>
2023-08-27 18:04:24 -0600

    roll version

--- legere_sales/__manifest__.py
+++ legere_sales/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Legere Sales',
-    'version': '1.0',
+    'version': '1.0.1',
     'category': 'Sales/Sales',
     'summary': """customization related to sales module""",
     'description': """
