6893c82c05a1a798258ad0b8b5a9164fe1723ed4
Sagar <sagar@dreammtn.services>
2023-12-05 13:46:00 +0530

    [ADD] Add create method for webhook hubspot contact

--- legere_hubspot/models/hubspot_connector.py
+++ legere_hubspot/models/hubspot_connector.py
@@ -1,3 +1,4 @@
+import ast
 
 from odoo import api, fields, models, _
 
@@ -8,8 +9,72 @@ class HubspotLog(models.Model):
 
     data = fields.Text(string='Data', required=True)
     error = fields.Text(string='Error')
-    is_updated = fields.Boolean(string='Updated')
+    is_updated = fields.Boolean(string='Created/Updated')
     record_id = fields.Many2one('res.partner', string='Record', required=False)
+
+    def update_contact(self):
+        contact_pool = self.env['res.partner'].sudo()
+        state_pool = self.env['res.country.state'].sudo()
+        country_pool = self.env['res.country'].sudo()
+        for record in self:
+            try:
+                data = ast.literal_eval(record.data)
+                contact = contact_pool.search([('hubspot_id', '=', data.get('objectId'))], limit=1)
+                updated = False
+                if contact:
+                    if data.get('propertyName') == 'firstname':
+                        name_parts = contact.name.split()
+                        if len(name_parts) >= 2:
+                            contact_name =  data.get('propertyValue') + ' ' + name_parts[1]
+                        else:
+                            contact_name =  data.get('propertyValue')
+                        contact.write({'name': contact_name})
+                        updated = True
+                    elif data.get('propertyName') == 'lastname':
+                        name_parts = contact.name.split()
+                        contact_name = name_parts[0] + ' ' + data.get('propertyValue')
+                        contact.write({'name': contact_name})
+                        updated = True
+                    elif data.get('propertyName') == 'email':
+                        contact.write({'email': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'phone':
+                        contact.write({'phone': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'jobtitle':
+                        contact.write({'function': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'mobilephone':
+                        contact.write({'mobile': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'website':
+                        contact.write({'website': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'address':
+                        contact.write({'street': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'zip':
+                        contact.write({'zip': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'city':
+                        contact.write({'city': data.get('propertyValue')})
+                        updated = True
+                    elif data.get('propertyName') == 'state':
+                        state_id = state_pool.search([('name', '=', data.get('propertyValue'))], limit=1)
+                        if state_id:
+                            contact.write({'state_id': state_id.id})
+                            updated = True
+                    elif data.get('propertyName') == 'country':
+                        country_id = country_pool.search(['|', ('name', '=', data.get('propertyValue')),('code', '=', data.get('propertyValue'))], limit=1)
+                        if country_id:
+                            contact.write({'country_id': country_id.id})
+                            updated = True
+                    if updated:
+                        record.sudo().write({'is_updated': True, 'record_id': contact.id})
+            except Exception as e:
+                record.sudo().write({
+                    'error': str(e),
+                })
 
 
 # class HubspotAccount(models.Model):
