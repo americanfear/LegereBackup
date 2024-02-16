d40eb799fea0e9529abf7004bdbed9fe32954e14
Bhaumik DMS <bhaumik@dreammtn.services>
2024-02-16 13:44:28 +0530

    [ADD]legere_pricelist_import_script: Added pricelist import script.

---
+++ legere_pricelist_import_script/credentials.py
@@ -0,0 +1,9 @@
+url = 'https://legerepharm.odoo.com'
+db = 'legerepharm-legere-master-7069524'
+user = 'API'
+token = '@m665cdx$dXZ8'
+
+# url = 'http://localhost:1603'
+# db = 'stride_barcode_03022024'
+# user = 'admin'
+# token = 'admin'
