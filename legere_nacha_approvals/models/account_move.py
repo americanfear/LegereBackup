0a870e8fdde66e9aa92f79e78477a0009311a498
Shawn Lonas <shawnlonas@gmail.com>
2024-05-29 14:37:46 -0700

    Changed write to work with multiple bill writes

--- legere_nacha_approvals/models/account_move.py
+++ legere_nacha_approvals/models/account_move.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 from odoo.exceptions import UserError
-# import logging
-# _logger = logging.getLogger(__name__)
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
@@ -64,67 +64,73 @@ class AccountMove(models.Model):
             template.send_mail(self.id)
 
     def write(self, vals):
-        needs_approval_notice = False
-        new_state = "state" in vals and vals["state"]
-        # If Vendor Bill and not internal change of Next Approval or state change to To Approve
-        if self.move_type == "in_invoice" and "next_approval_id" not in vals and "to_approve" not in vals:
-            next_amt = vals["amount_total"] if "amount_total" in vals else self.amount_total
-            new_approval = self.get_next_approval(False, next_amt)
-            approval_target = self.next_approval_id or new_approval
-            # If no Next Approval and it is not set before, ignore
-            if not approval_target:
-                if self.next_approval_id:
-                    vals["next_approval_id"] = False
-            # If amount_total is changing with this request, recalc Next Approval and move to Draft
-            elif next_amt != self.amount_total:
-                # Recalculate next approval with amount change
-                # ❓ Could only restart approval if the amount is more than before
-                vals["next_approval_id"] = new_approval
-                if new_state != "draft":
-                    vals["state"] = "draft"
-            # If new state is draft, reset the Next Approval
-            elif new_state == "draft":
-                vals["next_approval_id"] = new_approval
-            # If new state is posted, try to approve the Bill
-            elif new_state == "posted":
-                attempt_result = self.approval_attempt(approval_target, next_amt)
-                # If the current user is an approver
-                if attempt_result["result"]:
-                    next_approval = attempt_result["next_approval"]
-                    if next_approval and next_approval.sequence <= self.next_approval_id.sequence:
-                        raise UserError("This bill payment is awaiting the next level of approval.")
-                    elif next_approval:
-                        needs_approval_notice = True
-                        vals["state"] = "to_approve"
-                        vals["next_approval_id"] = next_approval
-                    else:
-                        # Only send notice if the current approver is NOT the original creator
-                        if self.env.user != self.create_uid:
-                            self.send_approval_notice()
+        _logger.warning(f'DEBUG>>> ACCOUNT MOVE WRITE self:{self}, vals:{vals}')
+        needs_approval_notice = {}
+        # TODO: If accept multiple, does it make sense to set next_approval_id and state for all???
+        for acct_move in self:
+            new_state = "state" in vals and vals["state"]
+            _logger.warning(f'DEBUG>>> move_type:{acct_move.move_type}, new_state:{new_state}')
+            # If Vendor Bill and not internal change of Next Approval or state change to To Approve
+            if acct_move.move_type == "in_invoice" and "next_approval_id" not in vals and new_state != "to_approve":
+                next_amt = vals["amount_total"] if "amount_total" in vals else acct_move.amount_total
+                new_approval = acct_move.get_next_approval(False, next_amt)
+                approval_target = acct_move.next_approval_id or new_approval
+                # If no Next Approval and it is not set before, ignore
+                if not approval_target:
+                    if acct_move.next_approval_id:
                         vals["next_approval_id"] = False
-                # If the current user is not an approver but approval is needed
-                elif attempt_result["next_approval"]:
-                    # If it is already in To Approve, then reject the attempt
-                    if self.state == "to_approve":
-                        raise UserError("You are not authorized to approve this payment.")
-                    # Otherwise it is someone posting it initially, so set to To Approve
-                    else:
-                        needs_approval_notice = True
-                        vals["state"] = "to_approve"
-                        vals["next_approval_id"] = attempt_result["next_approval"]
-            # If canceling the Bill, clear the Next Approval field
-            elif new_state == "cancel":
-                if self.next_approval_id:
-                    vals["next_approval_id"] = False
-            # Otherwise, update the Next Approval if moving to Draft or if it is remaining in Draft or To Approve
-            elif new_state == "draft" or (not new_state and self.state in ["draft", "to_approve"]):
-                if self.next_approval_id != new_approval:
+                # If amount_total is changing with this request, recalc Next Approval and move to Draft
+                elif next_amt != acct_move.amount_total:
+                    # Recalculate next approval with amount change
+                    # ❓ Could only restart approval if the amount is more than before
                     vals["next_approval_id"] = new_approval
+                    if new_state != "draft":
+                        vals["state"] = "draft"
+                # If new state is draft, reset the Next Approval
+                elif new_state == "draft":
+                    vals["next_approval_id"] = new_approval
+                # If new state is posted, try to approve the Bill
+                elif new_state == "posted":
+                    attempt_result = acct_move.approval_attempt(approval_target, next_amt)
+                    # If the current user is an approver
+                    if attempt_result["result"]:
+                        next_approval = attempt_result["next_approval"]
+                        if next_approval and next_approval.sequence <= acct_move.next_approval_id.sequence:
+                            raise UserError("This bill payment is awaiting the next level of approval.")
+                        elif next_approval:
+                            needs_approval_notice[acct_move.id] = True
+                            vals["state"] = "to_approve"
+                            vals["next_approval_id"] = next_approval
+                        else:
+                            # Only send notice if the current approver is NOT the original creator
+                            if self.env.user != acct_move.create_uid:
+                                acct_move.send_approval_notice()
+                            vals["next_approval_id"] = False
+                    # If the current user is not an approver but approval is needed
+                    elif attempt_result["next_approval"]:
+                        # If it is already in To Approve, then reject the attempt
+                        if acct_move.state == "to_approve":
+                            raise UserError("You are not authorized to approve this payment.")
+                        # Otherwise it is someone posting it initially, so set to To Approve
+                        else:
+                            needs_approval_notice[acct_move.id] = True
+                            vals["state"] = "to_approve"
+                            vals["next_approval_id"] = attempt_result["next_approval"]
+                # If canceling the Bill, clear the Next Approval field
+                elif new_state == "cancel":
+                    if acct_move.next_approval_id:
+                        vals["next_approval_id"] = False
+                # Otherwise, update the Next Approval if moving to Draft or if it is remaining in Draft or To Approve
+                elif new_state == "draft" or (not new_state and acct_move.state in ["draft", "to_approve"]):
+                    if acct_move.next_approval_id != new_approval:
+                        vals["next_approval_id"] = new_approval
 
-        move = super().write(vals)
+        super().write(vals)
+        # move = super().write(vals)
         # Only send approval request in To Approve stage
-        if needs_approval_notice and self.state == "to_approve" and (
-            self.next_approval_id and not self.next_approval_id.ignore_notification
-        ):
-            self.send_approval_request()
-        return move
+        for move in self:
+            if needs_approval_notice and move.id in needs_approval_notice and move.state == "to_approve" and (
+                move.next_approval_id and not move.next_approval_id.ignore_notification
+            ):
+                move.send_approval_request()
+        return
